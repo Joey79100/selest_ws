f9447a58e0438ff388504d173b586b515c5e2fad
Joey79100 <joey_quint@hotmail.fr>
2018-05-18 15:00:54 +0200

    Vérification que l'utilisateur est bien présent dans une conversation avant d'en afficher les messages (fix #8)

--- get_messages.php
+++ get_messages.php
@@ -28,40 +28,16 @@
 			$id_emetteur = $_SESSION['selest_ws']['uti_id'];
 		}
 
-		// Si on a un ID de message passé en paramètre, c'est qu'on veut les messages antérieurs à celui-ci
-		if(isset($_GET["id_message"])){
-			$id_message = $_GET["id_message"];
-			$query_where .= " AND mes_id < :id_message";
-			$parametres_additionnels[":id_message"] = $id_message;
-		}
-
-		// Si on a un nombre de messages indiqué, on le récupère, sinon on prend 10 par défaut
-		if(isset($_GET["nb_messages"])){
-			$nb_messages = $_GET["nb_messages"];
-		} else {
-			$nb_messages = 10;
-		}
-
-
-
-
-		/**
-		 * Récupération des utilisateurs participant
-		 */
 		
+		/**
+		 * Vérification que l'utlisateur fait bien partie de cette conversation
+		 */
+
 		// préparation de la requête
-		$query = "SELECT
-			uti_id,
-			uti_identifiant,
-			uti_droits,
-			adh_id,
-			adh_nom,
-			adh_prenom,
-			adh_ville
+		$query = "SELECT COUNT(*) AS utilisateur_present
 			FROM rel_conversation_utilisateur
-			INNER JOIN utilisateur ON uti_id = rcu_uti_id
-			LEFT JOIN adherent ON adh_id = uti_adh_id
 			WHERE rcu_con_id = :con_id
+			AND rcu_uti_id = :uti_id
 		";
 
 		$stmt = $db->database->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
@@ -69,88 +45,157 @@
 		try{
 
 			$stmt->execute($parametres = array(
-				':con_id' => $id_conversation
+				':con_id' => $id_conversation,
+				':uti_id' => $id_emetteur
 			));
-			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$utilisateur_present = $stmt->fetchAll(PDO::FETCH_ASSOC)[0]['utilisateur_present'];
 
-			$participants = array();
-			foreach($result as $row){
-				$participant = array();
-				$participant['utilisateur'] = array_filter_key_prefix($row, 'uti');
-				$participant['adherent'] = ($row['adh_id'] != null) ? array_filter_key_prefix($row, 'adh') : null;
-				$participants[] = $participant;
+		} catch(PDOException $e) {
+
+			stopWithError($e, "Erreur lors de la vérification de la présence de l'utilisteur dans la conversation");
+
+		}
+
+
+		
+		// On ne récupère les messages que si l'utilisateur fait partie de la conversation
+		if($utilisateur_present){
+
+
+			// Si on a un ID de message passé en paramètre, c'est qu'on veut les messages antérieurs à celui-ci
+			if(isset($_GET["id_message"])){
+				$id_message = $_GET["id_message"];
+				$query_where .= " AND mes_id < :id_message";
+				$parametres_additionnels[":id_message"] = $id_message;
 			}
 
-		} catch(PDOException $e) {
-
-			stopWithError($e, "Erreur lors de la récupération des participants");
-
-		}
-
-
-
+			// Si on a un nombre de messages indiqué, on le récupère, sinon on prend 10 par défaut
+			if(isset($_GET["nb_messages"])){
+				$nb_messages = $_GET["nb_messages"];
+			} else {
+				$nb_messages = 10;
+			}
 
 
 
-		/**
-		 * Récupération des messages
-		 */
-		
-		// préparation de la requête
-		$query = "SELECT mes_id,
-			mes_uti_id_emetteur,
-			mes_texte,
-			mes_date
-			FROM conversation
-			INNER JOIN message ON mes_con_id = con_id
-			WHERE con_id = :con_id
-			$query_where
-			ORDER BY mes_date DESC
-			LIMIT :nb_messages
-		";
 
-		$stmt = $db->database->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-		$parametres = array(
-			':con_id' => $id_conversation,
-			':nb_messages' => $nb_messages
-		);
-		$args = array_merge($parametres, $parametres_additionnels);
-
-
-		try{
-
-			$stmt->execute($parametres);
-			$messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-		} catch(PDOException $e) {
-
-			stopWithError($e, "Erreur lors de la récupération des messages");
-
-		}
-
-		$db->close();
-		
-		
-		
-		// récupération des résultats s'ils existent
-		if($stmt->rowCount() > 0){
-
-			// récupération des résultats
-			$response["participants"] = $participants;
-			$response["messages"] = $messages;
+			/**
+			 * Récupération des utilisateurs participant
+			 */
 			
-			// succès
-			$response["success"] = 1;
-			$code = CODE_OK;
+			// préparation de la requête
+			$query = "SELECT
+				uti_id,
+				uti_identifiant,
+				uti_droits,
+				adh_id,
+				adh_nom,
+				adh_prenom,
+				adh_ville
+				FROM rel_conversation_utilisateur
+				INNER JOIN utilisateur ON uti_id = rcu_uti_id
+				LEFT JOIN adherent ON adh_id = uti_adh_id
+				WHERE rcu_con_id = :con_id
+			";
 
+			$stmt = $db->database->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+			try{
+
+				$stmt->execute($parametres = array(
+					':con_id' => $id_conversation
+				));
+				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				$participants = array();
+				foreach($result as $row){
+					$participant = array();
+					$participant['utilisateur'] = array_filter_key_prefix($row, 'uti');
+					$participant['adherent'] = ($row['adh_id'] != null) ? array_filter_key_prefix($row, 'adh') : null;
+					$participants[] = $participant;
+				}
+
+			} catch(PDOException $e) {
+
+				stopWithError($e, "Erreur lors de la récupération des participants");
+
+			}
+
+
+
+
+
+
+			/**
+			 * Récupération des messages
+			 */
+			
+			// préparation de la requête
+			$query = "SELECT mes_id,
+				mes_uti_id_emetteur,
+				mes_texte,
+				mes_date
+				FROM conversation
+				INNER JOIN message ON mes_con_id = con_id
+				WHERE con_id = :con_id
+				$query_where
+				ORDER BY mes_date DESC
+				LIMIT :nb_messages
+			";
+
+			$stmt = $db->database->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+			$parametres = array(
+				':con_id' => $id_conversation,
+				':nb_messages' => $nb_messages
+			);
+			$args = array_merge($parametres, $parametres_additionnels);
+
+
+			try{
+
+				$stmt->execute($parametres);
+				$messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			} catch(PDOException $e) {
+
+				stopWithError($e, "Erreur lors de la récupération des messages");
+
+			}
+
+			$db->close();
+			
+			
+			
+			// récupération des résultats s'ils existent
+			if($stmt->rowCount() > 0){
+
+				// récupération des résultats
+				$response["participants"] = $participants;
+				$response["messages"] = $messages;
+				
+				// succès
+				$response["success"] = 1;
+				$code = CODE_OK;
+
+			} else {
+				
+				// pas de donnée
+				$response["success"] = 0;
+				$response["message"] = "Aucun résultat";
+				$code = CODE_NOT_FOUND;
+
+			}
+
+			
 		} else {
 			
 			// pas de donnée
 			$response["success"] = 0;
-			$response["message"] = "Aucun résultat";
-			$code = CODE_NOT_FOUND;
+			$response["message"] = "Requête impossible - l'utilisateur ne fait pas partie de la conversation";
+			$code = CODE_FORBIDDEN;
 
 		}
+
 
 	} else {
 
